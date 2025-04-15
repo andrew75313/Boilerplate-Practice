89b9bfa2c7d3ee94dfe7ab1af19ceb94675fa361
Andrew Kim <andrew75313@gmail.com>
2025-04-15 16:57:57 +0900

    first commit

---
+++ src/main/java/com/example/boilerplatepractice/BoilerplatePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.boilerplatepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplatePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoilerplatePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boilerplatepractice/BoilerplatePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boilerplatepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplatePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
